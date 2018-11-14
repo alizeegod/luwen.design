6755c7bb781ed30a623414ad82b026c5d02a63a7
alizeegod <zhangqiang@youshu.cc>
2018-11-14 19:46:49 +0800

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,204 @@
 <template>
     <section class="container">
-        <div>
-            <logo/>
-            <h1 class="title">
-                luwen.design
-            </h1>
-            <h2 class="subtitle">
-                luwen design
-            </h2>
-            <div class="links">
-                <a
-                    href="https://nuxtjs.org/"
-                    target="_blank"
-                    class="button--green">Documentation</a>
-                <a
-                    href="https://github.com/nuxt/nuxt.js"
-                    target="_blank"
-                    class="button--grey">GitHub</a>
-            </div>
-        </div>
+        <canvas width="1920" height="150"></canvas>
     </section>
 </template>
 
 <script>
-    import Logo from '~/components/Logo.vue'
 
     export default {
-        components: {
-            Logo
+        mounted() {
+
+            var TextParticles = function () {
+
+                var extend = function (target, source, isOverwrite) {
+                    if (isOverwrite == undefined) isOverwrite = true;
+                    for (var key in source) {
+                        if (!(key in target) || isOverwrite) {
+                            target[key] = source[key];
+                        }
+                    }
+                    return target;
+                };
+                var $ = function (id) {
+                    return document.getElementById(id) || id;
+                };
+                var $$ = function (tag, p) {
+                    return p.getElementsByTagName(tag);
+                };
+                var addEvent = function (o, e, f) {
+                    o.addEventListener ? o.addEventListener(e, f, false) : o.attachEvent('on'+e, function () {f.call(o)});
+                }
+                window.requesetAnimFrame = function () {
+                    return window.requesetAnimationFrame ||
+                        window.webkitRequestAnimationFrame ||
+                        window.mozRequestAnimationFrame ||
+                        window.oRequestAnimationFrame ||
+                        window.msRequestAnimationFrame ||
+                        function (fn) {
+                            window.setTimeout(fn, 1000/60);
+                        };
+                }();
+
+                var mouseX = 0, mouseY = 0;
+                function onMousemove (e) {
+                    mouseX = e.clientX;
+                    mouseY = e.clientY;
+                }
+                function onResize (e) {
+
+                }
+
+                var init = function (opt) {
+                    this.opt = {
+                        canvasId: null
+                    }
+                    this.place = 0;
+
+                    extend(this.opt, opt);
+
+                    this.initialize();
+                }
+                init.prototype = {
+                    initialize: function () {
+                        if (this.opt.canvasId == null) {
+                            this.genCanvas();
+                        } else {
+                            this.canvas = $(this.opt.canvasId);
+                        }
+
+                        this.particles = [];
+
+                        this.ctx = this.canvas.getContext('2d');
+                        this.setCvsStyle();
+                        this.fillText();
+                        this.getImageData();
+                        this.bindEvent();
+                        this.loop();
+                    },
+                    bindEvent: function () {
+                        var _this = this;
+                        addEvent(this.canvas, 'mousemove', onMousemove);
+                        addEvent(window, 'resize', function (e) {
+                            _this.setCvsStyle();
+                        });
+                    },
+                    loop: function () {
+                        var _this = this;
+                        /*setInterval(function () {
+                            _this.doFrame();
+                        }, 20);
+                        */
+                        requesetAnimFrame(function () {_this.loop()});
+                        this.doFrame();
+                    },
+                    doFrame: function () {
+                        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                        for (var i = 0; i < this.particles.length; i ++) {
+                            var p = this.particles[i];
+                            p.update();
+                        }
+                    },
+                    getImageData: function () {
+                        var imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+                        for (var x = 0; x < imageData.width; x ++) {
+                            for (var y = 0; y < imageData.height; y ++) {
+                                //var i = 4*(x * imageData.height + y);
+                                var i = 4*(y * imageData.width + x);
+                                if (imageData.data[i + 3] > 128) {
+                                    this.place ++;
+                                    (this.place%4 == 0) && this.particles.push(new Particle(x, y, this.canvas));
+                                }
+                            }
+                        }
+                    },
+                    fillText: function () {
+                        this.ctx.textAlign = 'center';
+                        this.ctx.textBaseline = 'middle';
+                        this.ctx.font = '200px sans-serif';
+                        this.ctx.fillText('陆雯', this.canvas.width/2, this.canvas.height/3);
+                    },
+                    setCvsStyle: function () {
+                        this.canvas.width = window.innerWidth;
+                        this.canvas.height = window.innerHeight;
+                    },
+                    genCanvas: function () {
+                        var canvas = document.createElement('canvas');
+                        document.body.appendChild(canvas);
+                        this.canvas = canvas;
+                        return canvas;
+                    }
+                };
+
+                var Particle = function (x, y, canvas) {
+                    this.endX = x;
+                    this.endY = y;
+                    this.canvas = canvas;
+                    this.ctx = this.canvas.getContext('2d');
+                    this.x = Math.random() * this.canvas.width;
+                    this.y = Math.random() * this.canvas.height;
+
+                    this.vx = Math.random()*10 - 5;
+                    this.vy = Math.random()*10 - 5;
+
+                }
+                Particle.prototype = {
+                    move: function () {
+                        var disX = this.endX - this.x;
+                        var disY = this.endY - this.y;
+                        var dis = Math.sqrt(Math.pow(disX, 2) + Math.pow(disY, 2));
+                        var force = dis * .01;
+                        var angle = Math.atan2(disY, disX); // atan2(x, y) 返回点(x, y)到x 轴的弧度
+
+                        var mouseF = 0, mouseA = 0;
+                        if (mouseX > 0 && mouseY > 0) {
+                            var dis = Math.pow((this.x - mouseX), 2) + Math.pow((this.y - mouseY), 2);
+                            mouseF = Math.min(5000/dis, 5000);
+                            mouseA = Math.atan2(this.y - mouseY, this.x - mouseX);
+                        } else {
+                            mouseF = 0;
+                            mouseA = 0;
+                        }
+
+                        this.vx += force * Math.cos(angle) + mouseF * Math.cos(mouseA);
+                        this.vy += force * Math.sin(angle) + mouseA * Math.sin(mouseA);
+
+                        this.vx *= 0.92;
+                        this.vy *= .92;
+
+                        //
+                        this.x += this.vx;
+                        this.y += this.vy;
+                    },
+                    render: function () {
+                        this.ctx.fillStyle = '#111';
+                        this.ctx.fillRect(this.x, this.y, 2, 2);
+                    },
+                    update: function () {
+                        this.move();
+                        this.render();
+                    }
+                }
+
+                return init;
+            }();
+
+            onload = function () {
+                new TextParticles();
+            }
+
         }
     }
 </script>
 
 <style>
-
-    .container {
-        min-height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
+    html, body {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        overflow:hidden;
+        margin: 0;
+        padding: 0;
     }
 
-    .title {
-        font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-        display: block;
-        font-weight: 300;
-        font-size: 100px;
-        color: #35495e;
-        letter-spacing: 1px;
-    }
-
-    .subtitle {
-        font-weight: 300;
-        font-size: 42px;
-        color: #526488;
-        word-spacing: 5px;
-        padding-bottom: 15px;
-    }
-
-    .links {
-        padding-top: 15px;
-    }
 </style>
